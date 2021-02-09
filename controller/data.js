f0c605e9ac703fd5aec67b9b85646a536486af92
Robin Bron <finwo@pm.me>
2021-02-09 20:54:24 +0100

    Add limit option to /data

--- controller/data.js
+++ controller/data.js
@@ -7,8 +7,17 @@ module.exports = [
     path: '/data',
     name: 'data.fetch',
     async handler(req, res, next) {
-      const since = req.body.since || 0;
-      return new HttpOk(app.history.filter(record => record.timestamp > since));
+      const since  = req.body.since || 0;
+      const limit  = req.body.limit || 100;
+      const result = [];
+
+      for(const record of app.history) {
+        if (result.length    >= limit) break;
+        if (record.timestamp <= since) continue;
+        result.push(record);
+      }
+
+      return new HttpOk(result);
     },
   },
 

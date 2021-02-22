a8dc246339bcf614745b060d4c7c4dc1c889e3aa
Robin Bron <finwo@pm.me>
2021-02-22 23:44:36 +0100

    Increase session duration to an hour

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   authseed : process.env.AUTH_SEED || 'change-me',
-  authlen  : 300,           // Sessions will be active for 5 minutes
+  authlen  : 3600,           // Sessions will be active for 5 minutes
   // authlen  : 3600 * 24 * 7, // Sessions will be active for a week
 
 

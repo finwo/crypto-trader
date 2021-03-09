9878e60a87d2bd62ecfd72841cbf7b9c1d3ce29e
Robin Bron <finwo@pm.me>
2021-03-09 23:27:26 +0100

    Change strategy to varchar(4096) for up-coming changes

--- model/portfolio.js
+++ model/portfolio.js
@@ -7,7 +7,7 @@ module.exports = sequelize => {
     name         : DataTypes.STRING,
     baseCurrency : DataTypes.STRING,
     exchange     : DataTypes.STRING,
-    strategy     : DataTypes.STRING,
+    strategy     : DataTypes.STRING(4096),
     tradegap     : DataTypes.DOUBLE,
     credentials  : DataTypes.STRING(1024),
     markets      : DataTypes.STRING(4096),
